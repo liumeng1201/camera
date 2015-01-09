745d1b2a03ad5d26da77dc4d78e793659d875389
liumeng <lmeng@qiujieda.com>
2015-01-09 10:45:46 +0800

    资源精简

--- OpenCamerasf/src/net/sourceforge/opencamera/CameraActivityNew.java
+++ OpenCamerasf/src/net/sourceforge/opencamera/CameraActivityNew.java
@@ -58,7 +58,7 @@ public class CameraActivityNew extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_camera_new);
 
 		setWindowFlagsForCamera();
 
